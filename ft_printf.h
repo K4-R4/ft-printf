34b8056acbeacd0e07338d0e6ebe9f7ce32266c1
K4R4 <mabdman512@gmail.com>
2023-05-30 03:31:28 +0900

    enum struct

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 18:03:12 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/05/29 23:43:57 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/05/30 03:30:58 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,26 @@
 # include "./libft/libft.h"
 # include <stdarg.h>
 
-int		ft_printf(const char *fmt, ...);
-size_t	ft_printf_putchar(char c);
-size_t	ft_printf_putstr(char *s);
-size_t	ft_putnbr_base(long long nbr, char *base);
+typedef enum e_flag
+{
+	HYPHEN = 1 << 0,
+	ZERO = 1 << 1,
+	PERIOD = 1 << 2,
+	HASH = 1 << 3,
+	SPACE = 1 << 4,
+	PLUS = 1 << 5
+}				t_flag;
+
+typedef struct s_placeholder
+{
+	char		flags;
+	size_t		field_width;
+	size_t		precision;
+}				t_placeholder;
+
+int				ft_printf(const char *fmt, ...);
+size_t			ft_printf_putchar(char c);
+size_t			ft_printf_putstr(char *s);
+size_t			ft_putnbr_base(long long nbr, char *base);
 
 #endif
