2dd5b4f71e7593abdd49ef22e8ae592b9bb9745d
K4R4 <mabdman512@gmail.com>
2023-05-31 18:04:12 +0900

    bonus #

--- ft_printf_uint.c
+++ ft_printf_uint.c
@@ -14,10 +14,21 @@
 
 size_t	ft_printf_uint(unsigned long long nbr, t_placeholder ph)
 {
+	size_t l;
+
+	l = 0;
 	if (ph.type == U_DEC)
 		return (ft_putnbr_base(nbr, DECIMAL));
 	else if (ph.type == HEX_L)
-		return (ft_putnbr_base(nbr, HEXADECIMAL_L));
+	{
+		if (nbr && ph.flags & HASH)
+			l += ft_putstr_r("0x");
+		return (l + ft_putnbr_base(nbr, HEXADECIMAL_L));
+	}
 	else
-		return (ft_putnbr_base(nbr, HEXADECIMAL_U));
+	{
+		if (nbr && ph.flags & HASH)
+			l += ft_putstr_r("0X");
+		return (l + ft_putnbr_base(nbr, HEXADECIMAL_U));
+	}
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 20:17:30 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/05/30 23:10:30 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/05/31 18:02:44 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,12 @@ int	main(void)
 	printf("==========%%#x==========\n");
 	printf("%d\n", printf("%#x\n", 256));
 	printf("%d\n", ft_printf("%#x\n", 256));
+	printf("%d\n", printf("%#x\n", 0));
+	printf("%d\n", ft_printf("%#x\n", 0));
 	printf("==========%%#X==========\n");
 	printf("%d\n", printf("%#X\n", 256));
 	printf("%d\n", ft_printf("%#X\n", 256));
+	printf("%d\n", printf("%#X\n", 0));
+	printf("%d\n", ft_printf("%#X\n", 0));
 	return (0);
 }
